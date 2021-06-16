8d33996afafc1e1314514aa6eaf6f1a99a24d26c
Zhuojia Shen <chaosdefinition@hotmail.com>
2021-06-16 14:18:32 -0400

    Add a script to compile and run PinLock.

---
+++ scripts/pinlock.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+ROOT_DIR=`dirname $0 | sed 's/$/\/../' | xargs realpath`
+
+PROJ=pinlock
+PROJ_DIR="$ROOT_DIR/workspace/$PROJ"
+RUN_CFG="$PROJ_DIR/$PROJ.cfg"
+
+PROGRAMS=(
+    "pinlock"
+)
+
+CONFIGURATIONS=(
+    "baseline"
+    "ss"
+    "sp"
+    "cfi"
+    "silhouette"
+    "invert"
+    "sfifull"
+)
+
+#
+# Load common components.
+#
+. "$ROOT_DIR/scripts/common.sh"
+
+#
+# Entrance of the script.
+#
+case $1 in
+"run" )
+    if (( $# == 2 )); then
+        for program in ${PROGRAMS[@]}; do
+            run $2 $program "Elapsed time" "$ROOT_DIR/scripts/pinlock-input.txt" 20
+        done
+    else
+        run $2 $3 "Elapsed time" "$ROOT_DIR/scripts/pinlock-input.txt" 20
+    fi
+    ;;
+* )
+    if (( $# == 1 )); then
+        # Compile each benchmark program
+        for program in ${PROGRAMS[@]}; do
+            compile $1 $program
+        done
+
+        echo Done
+    else
+        compile $1 $2
+    fi
+    ;;
+esac
